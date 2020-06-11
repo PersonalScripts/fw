6106d9b0d281c0b4fd47793f0224bffffaffcce8
pvp2004 <pqdplay@gmail.com>
2020-06-11 09:27:14 -0300

    Update Criador_de_Titulos_FW.user.js

--- Criador_de_Titulos_FW.user.js
+++ Criador_de_Titulos_FW.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Criador de Títulos [FW]
 // @namespace   PvP
-// @version      1.13
+// @version      1.14
 // @description  Busca as informações e preenche o postador.
 // @author      PvP
 // @include     https://filewarez.tv/postador.php?do=addtitle&step=2&type=movie
